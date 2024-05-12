19d915599348350b52f72e114516f75f271d7ebe
EvoShishir <atef.arman@yahoo.com>
2024-05-12 22:38:03 +0600

    fixed type issue

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -3,7 +3,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { hotelReducer } from "./features/bookHotel";
 
-const reducers = combineReducers({
+const reducers: any = combineReducers({
   hotels: hotelReducer,
 });
 
